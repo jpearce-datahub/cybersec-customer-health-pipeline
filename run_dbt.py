bce5128171b29815697fc817fe2828d32f3f984d
jpearce-datahub <pearcejl@live.com>
2025-11-16 11:37:19 -0500

    Move Python utility scripts to scripts directory

--- run_dbt.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-os.chdir('dbt')
-result = subprocess.run(['python', '-c', 'import dbt.cli.main; dbt.cli.main.cli()', 'run', '--profiles-dir', '.'], 
-                       capture_output=True, text=True)
-print(result.stdout)
-if result.stderr:
-    print("STDERR:", result.stderr)
